d72701c8b9bb6b5dc525984b6df99d5d39c0c132
BukenyaLukman <bukenyalukman@gmail.com>
2020-06-06 15:12:06 +0300

    Arithmetic

---
+++ Arithmetic.js
@@ -0,0 +1,16 @@
+// console.log(13.5 / 3);
+// console.log(5 - 4);
+// console.log(5 * 4);
+// console.log(5 + 4);
+// console.log(5 % 4);
+
+
+let value = 2
+
+value++
+
+console.log(value)
+
+--value;
+
+console.log(value)
